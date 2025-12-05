3b3c9ec34488ffbd3c25a88bf5784c73c8744eaa
AlvaroLucini <alvaro.lucini@ingka.ikea.com>
2025-12-05 07:51:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -25,14 +25,14 @@ st.set_page_config(page_title="IVR Tester", page_icon="📞", layout="centered")
 # MODO DEBUG (para ti)
 # =========================
 
-DEBUG_MODE = bool(st.secrets.get("debug_mode", False))
+DEBUG_MODE = bool(st.secrets.get("debug_mode", True))
 
 # =========================
 # MODO MANTENIMIENTO
 # =========================
 # Si MAINTENANCE_MODE está a True (o en secrets), la app muestra solo
 # un mensaje + imagen y NO permite lanzar tests (salvo que estés en DEBUG).
-MAINTENANCE_MODE = bool(st.secrets.get("maintenance_mode", True))
+MAINTENANCE_MODE = bool(st.secrets.get("maintenance_mode", False))
 
 MAINTENANCE_MESSAGE = (
     "Estamos actualizando la IVR para testar una nueva estructura. "
@@ -1355,6 +1355,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
